e77a7bbc561e616e094e9c2549fa997556c76b6c
Rafael <rafae.uribe.2016@upb.edu.co>
2019-09-17 17:14:19 -0500

    React: agragar Footer
    y otros arreglos menores

--- src/App.js
+++ src/App.js
@@ -7,65 +7,27 @@ import SearchBar from './components/searchBar';
 import Indicators from './components/indicator';
 import Graphs from './components/graphs';
 import Tables from './components/tables';
+import Footer from './components/footer';
 
 function App() {
   return (
     <div className="App">
-        <div>
-        <div className="wrapper ">
-          <SideBarMenu/>
-          <div className="main-panel">
-            <SearchBar/>
-            <div className="content">
-              <div className="container-fluid">
-                <Indicators/>
-                <Graphs/>
-                <Tables/>
-              </div>
+      <div className="wrapper ">
+        <SideBarMenu />
+        <div className="main-panel">
+          <SearchBar />
+          <div className="content">
+            <div className="container-fluid">
+              <Indicators />
+              <Graphs />
+              <Tables />
             </div>
-            <footer className="footer">
-              <div className="container-fluid">
-                <nav className="float-left">
-                  <ul>
-                    <li>
-                      <a href="https://www.creative-tim.com">Creative Tim</a>
-                    </li>
-                    <li>
-                      <a href="https://creative-tim.com/presentation">
-                        About Us
-                      </a>
-                    </li>
-                    <li>
-                      <a href="http://blog.creative-tim.com">Blog</a>
-                    </li>
-                    <li>
-                      <a href="https://www.creative-tim.com/license">
-                        Licenses
-                      </a>
-                    </li>
-                  </ul>
-                </nav>
-                <div className="copyright float-right">
-                  &copy;
-                  <script>document.write(new Date().getFullYear())</script>,
-                  made with <i className="material-icons">favorite</i> by
-                  <a href="https://www.creative-tim.com" target="_blank">
-                    Creative Tim
-                  </a>{" "}
-                  for a better web.
-                </div>
-              </div>
-            </footer>
           </div>
+          <Footer />
         </div>
-        <div className="fixed-plugin">
-          <div className="dropdown show-dropdown">
-            <a href="#" data-toggle="dropdown">
-              <i className="fa fa-cog fa-2x"> </i>
-            </a>
-            <DropDownMenu/>
-          </div>
-        </div>
+      </div>
+      <div className="fixed-plugin">
+        <DropDownMenu />
       </div>
     </div>
   );
--- src/components/dropDownMenu.js
+++ src/components/dropDownMenu.js
@@ -3,119 +3,136 @@ import React, { Component } from 'react';
 class DropDownMenu extends Component{
   render(){
     return (
-      <div>
-        <ul className="dropdown-menu">
-          <li className="header-title"> Sidebar Filters</li>
-          <li className="adjustments-line">
-            <a
-              href="javascript:void(0)"
-              className="switch-trigger active-color"
-            >
-              <div className="badge-colors ml-auto mr-auto">
-                <span
-                  className="badge filter badge-purple"
-                  data-color="purple"
-                ></span>
-                <span
-                  className="badge filter badge-azure"
-                  data-color="azure"
-                ></span>
-                <span
-                  className="badge filter badge-green"
-                  data-color="green"
-                ></span>
-                <span
-                  className="badge filter badge-warning"
-                  data-color="orange"
-                ></span>
-                <span
-                  className="badge filter badge-danger"
-                  data-color="danger"
-                ></span>
-                <span
-                  className="badge filter badge-rose active"
-                  data-color="rose"
-                ></span>
-              </div>
-              <div className="clearfix"></div>
-            </a>
-          </li>
-          <li className="header-title">Images</li>
-          <li className="active">
-            <a className="img-holder switch-trigger" href="javascript:void(0)">
-              <img src="../assets/img/sidebar-1.jpg" alt="" />
-            </a>
-          </li>
-          <li>
-            <a className="img-holder switch-trigger" href="javascript:void(0)">
-              <img src="../assets/img/sidebar-2.jpg" alt="" />
-            </a>
-          </li>
-          <li>
-            <a className="img-holder switch-trigger" href="javascript:void(0)">
-              <img src="../assets/img/sidebar-3.jpg" alt="" />
-            </a>
-          </li>
-          <li>
-            <a className="img-holder switch-trigger" href="javascript:void(0)">
-              <img src="../assets/img/sidebar-4.jpg" alt="" />
-            </a>
-          </li>
-          <li className="button-container">
-            <a
-              href="https://www.creative-tim.com/product/material-dashboard"
-              target="_blank"
-              className="btn btn-primary btn-block"
-            >
-              Free Download
-            </a>
-          </li>
-          <li className="header-title">Want more components?</li>
-          <li className="button-container">
-            <a
-              href="https://www.creative-tim.com/product/material-dashboard-pro"
-              target="_blank"
-              className="btn btn-warning btn-block"
-            >
-              Get the pro version
-            </a>
-          </li>{" "}
-          -->
-          <li className="button-container">
-            <a
-              href="https://demos.creative-tim.com/material-dashboard/docs/2.1/getting-started/introduction.html"
-              target="_blank"
-              className="btn btn-default btn-block"
-            >
-              View Documentation
-            </a>
-          </li>
-          <li className="button-container github-star">
-            <a
-              className="github-button"
-              href="https://github.com/creativetimofficial/material-dashboard"
-              data-icon="octicon-star"
-              data-size="large"
-              data-show-count="true"
-              aria-label="Star ntkme/github-buttons on GitHub"
-            >
-              Star
-            </a>
-          </li>
-          <li className="header-title">Thank you for 95 shares!</li>
-          <li className="button-container text-center">
-            <button id="twitter" className="btn btn-round btn-twitter">
-              <i className="fa fa-twitter"></i> &middot; 45
-            </button>
-            <button id="facebook" className="btn btn-round btn-facebook">
-              <i className="fa fa-facebook-f"></i> &middot; 50
-            </button>
-            <br />
-            <br />
-          </li>
-        </ul>
+      <div className="dropdown show-dropdown">
+        <a href="#" data-toggle="dropdown">
+          <i className="fa fa-cog fa-2x"> </i>
+        </a>
+        <div>
+          <ul className="dropdown-menu">
+            <li className="header-title"> Sidebar Filters</li>
+            <li className="adjustments-line">
+              <a
+                href="javascript:void(0)"
+                className="switch-trigger active-color"
+              >
+                <div className="badge-colors ml-auto mr-auto">
+                  <span
+                    className="badge filter badge-purple"
+                    data-color="purple"
+                  ></span>
+                  <span
+                    className="badge filter badge-azure"
+                    data-color="azure"
+                  ></span>
+                  <span
+                    className="badge filter badge-green"
+                    data-color="green"
+                  ></span>
+                  <span
+                    className="badge filter badge-warning"
+                    data-color="orange"
+                  ></span>
+                  <span
+                    className="badge filter badge-danger"
+                    data-color="danger"
+                  ></span>
+                  <span
+                    className="badge filter badge-rose active"
+                    data-color="rose"
+                  ></span>
+                </div>
+                <div className="clearfix"></div>
+              </a>
+            </li>
+            <li className="header-title">Images</li>
+            <li className="active">
+              <a
+                className="img-holder switch-trigger"
+                href="javascript:void(0)"
+              >
+                <img src="../assets/img/sidebar-1.jpg" alt="" />
+              </a>
+            </li>
+            <li>
+              <a
+                className="img-holder switch-trigger"
+                href="javascript:void(0)"
+              >
+                <img src="../assets/img/sidebar-2.jpg" alt="" />
+              </a>
+            </li>
+            <li>
+              <a
+                className="img-holder switch-trigger"
+                href="javascript:void(0)"
+              >
+                <img src="../assets/img/sidebar-3.jpg" alt="" />
+              </a>
+            </li>
+            <li>
+              <a
+                className="img-holder switch-trigger"
+                href="javascript:void(0)"
+              >
+                <img src="../assets/img/sidebar-4.jpg" alt="" />
+              </a>
+            </li>
+            <li className="button-container">
+              <a
+                href="https://www.creative-tim.com/product/material-dashboard"
+                target="_blank"
+                className="btn btn-primary btn-block"
+              >
+                Free Download
+              </a>
+            </li>
+            <li className="header-title">Want more components?</li>
+            <li className="button-container">
+              <a
+                href="https://www.creative-tim.com/product/material-dashboard-pro"
+                target="_blank"
+                className="btn btn-warning btn-block"
+              >
+                Get the pro version
+              </a>
+            </li>{" "}
+            -->
+            <li className="button-container">
+              <a
+                href="https://demos.creative-tim.com/material-dashboard/docs/2.1/getting-started/introduction.html"
+                target="_blank"
+                className="btn btn-default btn-block"
+              >
+                View Documentation
+              </a>
+            </li>
+            <li className="button-container github-star">
+              <a
+                className="github-button"
+                href="https://github.com/creativetimofficial/material-dashboard"
+                data-icon="octicon-star"
+                data-size="large"
+                data-show-count="true"
+                aria-label="Star ntkme/github-buttons on GitHub"
+              >
+                Star
+              </a>
+            </li>
+            <li className="header-title">Thank you for 95 shares!</li>
+            <li className="button-container text-center">
+              <button id="twitter" className="btn btn-round btn-twitter">
+                <i className="fa fa-twitter"></i> &middot; 45
+              </button>
+              <button id="facebook" className="btn btn-round btn-facebook">
+                <i className="fa fa-facebook-f"></i> &middot; 50
+              </button>
+              <br />
+              <br />
+            </li>
+          </ul>
+        </div>
       </div>
-    );
+      );
     }
   }
   
